036b363cea94726c906b7de4b8c73b32d1961c91
alisha0906 <alisha14556@gmail.com>
2025-07-27 19:11:57 +0530

    Fix : remove unnecessary characters in regex

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,9 @@
 export const checkValidData = (email, password) => {
+  const isEmailValid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+  const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
 
-    const isEmailValid = /^[a-zA-Z0-9_.+\-]+[\x40][a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/.test(email);
-    const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+  if (!isEmailValid) return "Email entered is not valid";
+  if (!isPasswordValid) return "Password is not valid";
 
-    if(!isEmailValid) return "Email entered is not valid";
-    if(!isPasswordValid) return "Password is not valid";
-
-    return null;
-}
+  return null;
+};
